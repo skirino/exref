d80dd137c51968df798ae7f0c543bec486b59ce5
Paweł Drygas <pdrygas@seoway.pl>
2016-04-22 12:09:26 +0200

    Refactor warning's comment generation

--- lib/exref/mix/tasks/exref.ex
+++ lib/exref/mix/tasks/exref.ex
@@ -40,18 +40,14 @@ defmodule Mix.Tasks.Compile.Exref do
     pos <> gen_comment_txt(check, source, target)
   end
 
-  def gen_comment_txt(check, {sm, sf, sa}, tmfa) do
-    sm = sm |> to_string |> String.replace("Elixir.", "")
-    gen_comment_txt(check, "#{sm}.#{sf}/#{sa}", tmfa)
-  end
-  def gen_comment_txt(check, smfa, {tm, tf, ta}) do
-    tm = tm |> to_string |> String.replace("Elixir.", "")
-    gen_comment_txt(check, smfa, "#{tm}.#{tf}/#{ta}")
-  end
-  def gen_comment_txt(:undefined_function_calls, smfa, tmfa),  do: "#{smfa} calls undefined function #{tmfa}"
-  def gen_comment_txt(:undefined_functions, smfa, _tmfa),      do: "#{smfa} is not defined as a function"
-  def gen_comment_txt(:locals_not_used, smfa, _tmfa),          do: "#{smfa} is an unused local function"
-  def gen_comment_txt(:exports_not_used, smfa, _tmfa),         do: "#{smfa} is an unused export"
-  def gen_comment_txt(:deprecated_function_calls, smfa, tmfa), do: "#{smfa} calls deprecated function #{tmfa}"
-  def gen_comment_txt(:deprecated_functions, smfa, _tmfa),     do: "#{smfa} is deprecated"
+  defp gen_comment_txt(check, {sm, sf, sa}, tmfa), do: gen_comment_txt(check, mfa_to_string(sm, sf, sa), tmfa)
+  defp gen_comment_txt(check, smfa, {tm, tf, ta}), do: gen_comment_txt(check, mfa, mfa_to_string(tm, tf, ta))
+  defp gen_comment_txt(:undefined_function_calls, smfa, tmfa),  do: "#{smfa} calls undefined function #{tmfa}"
+  defp gen_comment_txt(:undefined_functions, smfa, _tmfa),      do: "#{smfa} is not defined as a function"
+  defp gen_comment_txt(:locals_not_used, smfa, _tmfa),          do: "#{smfa} is an unused local function"
+  defp gen_comment_txt(:exports_not_used, smfa, _tmfa),         do: "#{smfa} is an unused export"
+  defp gen_comment_txt(:deprecated_function_calls, smfa, tmfa), do: "#{smfa} calls deprecated function #{tmfa}"
+  defp gen_comment_txt(:deprecated_functions, smfa, _tmfa),     do: "#{smfa} is deprecated"
+
+  defp mfa_to_string(m, f, a), do: "#{m |> to_string |> String.replace("Elixir.", "")}.#{f}/#{a}"
 end
