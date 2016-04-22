a077d3656f3315350216deb6a6ca182118ec0c59
Paweł Drygas <pdrygas@seoway.pl>
2016-04-22 11:35:58 +0200

    Report more details and use human-readable format
    Makes the reported warnings user-friendly and gives them more details,
    like exact file and line of occurrences.
    To do this, Inaka's _xref\_runner_ is used instead bare _xref_ module.

--- lib/exref/mix/tasks/exref.ex
+++ lib/exref/mix/tasks/exref.ex
@@ -3,17 +3,55 @@ defmodule Mix.Tasks.Compile.Exref do
 
   @shortdoc "Check all function calls using xref"
 
+  @default_checks ~w/undefined_function_calls locals_not_used deprecated_function_calls/a
+
   def run(_args) do
     app_name = Mix.Project.config[:app] |> Atom.to_string
     ebin_dir = Path.join([Mix.Project.build_path, "lib", app_name, "ebin"]) |> String.to_char_list
-    results = :xref.d(ebin_dir)
-    Enum.each(results, &print_and_halt_on_nonempty/1)
+
+    results = for c <- @default_checks do
+      :xref_runner.check(c, %{dirs: [ebin_dir]})
+    end
+
+    results
+    |> List.flatten
+    |> print_and_raise_on_problems
   end
 
-  defp print_and_halt_on_nonempty({_, []}), do: nil
-  defp print_and_halt_on_nonempty({label, funcalls}) do
-    IO.puts("exref error: #{label}")
-    Enum.each(funcalls, fn f -> IO.puts("  #{inspect f}") end)
+  defp print_and_raise_on_problems([]), do: nil
+  defp print_and_raise_on_problems(problems) do
+    problems
+    |> Enum.map(&gen_comment/1)
+    |> Enum.each(&IO.puts/1)
+
     Mix.raise "Error in xref check! Please fix any errors and try again."
   end
+
+  defp gen_comment(xref_warn) do
+    %{filename: filename, line: line, source: source, check: check} = xref_warn
+    target = xref_warn[:target]
+
+    pos = case {filename, line} do
+            {"", _} -> ""
+            {_, 0}  -> "#{filename} "
+            {_, _}  -> "#{filename}:#{line} "
+          end
+
+    pos <> gen_comment_txt(check, source, target)
+  end
+
+  def gen_comment_txt(check, {sm, sf, sa}, tmfa) do
+    sm = sm |> to_string |> String.replace("Elixir.", "")
+    gen_comment_txt(check, "#{sm}.#{sf}/#{sa}", tmfa)
+  end
+  def gen_comment_txt(check, smfa, {tm, tf, ta}) do
+    tm = tm |> to_string |> String.replace("Elixir.", "")
+    gen_comment_txt(check, smfa, "#{tm}.#{tf}/#{ta}")
+  end
+  def gen_comment_txt(:undefined_function_calls, smfa, tmfa),  do: "#{smfa} calls undefined function #{tmfa}"
+  def gen_comment_txt(:undefined_functions, smfa, _tmfa),      do: "#{smfa} is not defined as a function"
+  def gen_comment_txt(:locals_not_used, smfa, _tmfa),          do: "#{smfa} is an unused local function"
+  def gen_comment_txt(:exports_not_used, smfa, _tmfa),         do: "#{smfa} is an unused export"
+  def gen_comment_txt(:deprecated_function_calls, smfa, tmfa), do: "#{smfa} calls deprecated function #{tmfa}"
+  def gen_comment_txt(:deprecated_functions, smfa, _tmfa),     do: "#{smfa} is deprecated"
 end
